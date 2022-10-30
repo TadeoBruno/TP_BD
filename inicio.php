9f52877fae42cbbfc58339c383cc3632c355782d
Tadeo <tadeobruno.99@gmail.com>
2022-10-30 19:21:16 -0300

    tablero 0.2v

--- inicio.php
+++ inicio.php
@@ -1,4 +1,5 @@
 <?php
+   require "./administrador/database.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,9 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WeatherNow</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css.crdownload"/>
-	<link rel="stylesheet" type="text/css" href="css/estilos.css"/>
-    <link rel="stylesheet" href="./css/estilos.css"/>
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
 <img src="img/weathernow.png"> 
@@ -21,14 +20,34 @@
 		<td align="left"><span>Ventas -</span> <a href="" style=" color:black; ">Ver detalle...</a></td>
 	</tr>
 </table>	
-<br>
-<table border="1" cellpadding="0" cellspacing="0" width="70%" align="center">
-	<tr>
-		<td width="10%"><b style=" color:black; ">Monto</b></td>
-		<td width="10%"><b style=" color:black; ">Objetivo</b></td>
-		<td width="10%"><b style=" color:black; ">Desvio</b></td>
-		<td><b style=" color:black; ">Estado</b></td>
-	</tr>
+</br>
+<table>
+    <div id="main-container">
+	    <thead>
+	       <tr>
+	    	   <th>Ciudad</th><th>Temperatura promedio mensual(°C)</th><th>Desvio</th><th>Estado</th>
+	       </tr>
+        </thead>
+	    <?php
+	       $sql="SELECT * FROM ciudades";
+	       $result=mysqli_query($conexion, $sql);
+    
+	       while($mostrar=mysqli_fetch_array($result)){
+	    ?>
+	    <tr>
+	    	<td><?php echo $mostrar['nombre'] ?></td><td><?php echo $mostrar['temperatura'] ?></td><td></td><td><?php if ($mostrar['temperatura'] > 17 and $mostrar['temperatura'] < 25){
+	    		           echo "Temperatura ideal";
+	    	          } if ($mostrar['temperatura'] < 10 || $mostrar['temperatura'] > 30) {
+	    				echo "Temperatura peligrosa";
+	    			  } if ($mostrar['temperatura'] > 9 and $mostrar['temperatura'] < 31)  {
+						echo "Temperatura poco peligrosa";
+					  }?></td>
+		</tr>
+	    <?php
+	       }
+	    ?>
+	</div>
+</table>
     
 </body>
 </html>
